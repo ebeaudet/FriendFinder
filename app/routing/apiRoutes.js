c9aa9d47cec5f258b273238bf26e9ed4e53f4fc0
unknown <ed.beaudet@gmail.com>
2019-02-15 21:33:18 -0500

    add new friend to the friends array

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,37 +1,17 @@
 var friends = require("../data/friends");
+console.log(friends);
 
 module.exports = function (app) {
-    
+    console.log("Do we get here????")
 app.get("/api/friends", function (req, res) {
     res.json(friends);
 });
 
     app.post("/api/friends", function (req, res) {
-        console.log(newFriend);
-        friends.push(newFriend);
-        res.json(newFriend);
+        console.log("friends");
+        console.log(req.body);
+        friends.push(req.body);
+        res.json(friends);
     });
 
 };
-
-// $("#submit").on("click", function (event) {
-//     event.preventDefault();
-
-//     var newFriend = {
-//         name: $("#name").val().trim(),
-//         photo: $("#photo").val().trim(),
-//         scores: [
-//             $("#question1").val(),
-//             $("#question2").val(),
-//             $("#question3").val(),
-//             $("#question4").val(),
-//             $("#question5").val(),
-//             $("#question6").val(),
-//             $("#question7").val(),
-//             $("#question8").val(),
-//             $("#question9").val(),
-//             $("#question10").val()
-//         ]
-//     };
-
-// })
