c8d4a4e812422b6d9aa0d018481bcb56eb735f10
unknown <ed.beaudet@gmail.com>
2019-02-17 16:43:10 -0500

    fixed padding on survey

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,10 +8,7 @@ app.get("/api/friends", function (req, res) {
 });
 
     app.post("/api/friends", function (req, res) {
-        console.log("friends");
-        console.log(req.body);
         friends.push(req.body);
         res.json(friends);
     });
-
-};
+}
