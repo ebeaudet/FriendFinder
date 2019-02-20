fa8f854ac17040573948732ff5f33d9599bea3c2
unknown <ed.beaudet@gmail.com>
2019-02-20 12:52:08 -0500

    clear the form

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,37 +1,54 @@
 
 var friends =
-[{
-    "name":"Cersei",
-    "photo":"https://baronmag.ca/.image/t_share/MTQ4MzYwNDkzMjAyNDgyMzM3/14463095_1314610621906610_5096555417063465197_n.png",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1 
-    ]     
+[
+  {
+      "name": "Cersei",
+      "photo": "https://baronmag.ca/.image/t_share/MTQ4MzYwNDkzMjAyNDgyMzM3/14463095_1314610621906610_5096555417063465197_n.png",
+      "scores": [
+          5,
+          3,
+          4,
+          2,
+          1,
+          1,
+          3,
+          4,
+          2,
+          1
+      ]
   },
   {
-    "name":"Concert Going Catie",
-    "photo":"http://www3.pictures.zimbio.com/gi/Festival+goers+watch+Ben+Howard+uwuwppx1MIHm.jpg",
-    "scores":[
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
+      "name": "Concert Going Catie",
+      "photo": "http://www3.pictures.zimbio.com/gi/Festival+goers+watch+Ben+Howard+uwuwppx1MIHm.jpg",
+      "scores": [
+          2,
+          3,
+          1,
+          4,
+          2,
+          3,
+          2,
+          4,
+          3,
+          2
+      ]
+  },
+  {
+      "name": "Ed",
+      "photo": "https://photos.google.com/photo/AF1QipMhLHkcl0QFZ7gCHWf6VlQU5DmlYvkqXetHVXhp",
+      "scores": [
+          5,
+          5,
+          5,
+          4,
+          3,
+          5,
+          3,
+          2,
+          4,
+          5
+      ]
   }
-  ]
+]
 
   module.exports = friends;
